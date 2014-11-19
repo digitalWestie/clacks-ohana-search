9531703f6824f1a63c0a58c89a415837ed9a834e
Rory Gianni <giannichan@gmail.com>
2014-11-19 10:46:06 +0000

    adds services controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Clackskids::Application.routes.draw do
+
+  get 'services'
+  get 'services/:id'
+
+  root to: 'services#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
