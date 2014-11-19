1583f2a0df5ee8dc5a029d77d93f2e755a498080
Rory Gianni <giannichan@gmail.com>
2014-11-19 11:59:27 +0000

    adds map and slider js to results

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,18 +1,19 @@
-/*var map = L.map('mapResults').setView([51.505, -0.09], 13);
+$(document).ready(function(){
+  var map = L.map('mapResults').setView([51.505, -0.09], 13);
 
-// Fixing the height of panels on the Search Page
-if ($(window).width() > 960) {
-  var windowHeight = $(window).height();
-  var minusFooterHeader = ($('.navbar-fixed-top').outerHeight() + $('footer').outerHeight());
-  $('#resultsPage').css({height: windowHeight - minusFooterHeader});
-}
+  // Fixing the height of panels on the Search Page
+  if ($(window).width() > 960) {
+    var windowHeight = $(window).height();
+    var minusFooterHeader = ($('.navbar-fixed-top').outerHeight() + $('footer').outerHeight());
+    $('#resultsPage').css({height: windowHeight - minusFooterHeader});
+  }
 
-if ($(window).width() < 768) {
-var windowHeight = $(window).height();
-var minusHeader = $('.navbar-fixed-top').outerHeight();
-$('#resultsPage').css({height: windowHeight - minusHeader});
-}
+  if ($(window).width() < 768) {
+    var windowHeight = $(window).height();
+    var minusHeader = $('.navbar-fixed-top').outerHeight();
+    $('#resultsPage').css({height: windowHeight - minusHeader});
+  }
 
-
-$("#yourBudget").slider({});
-$("#childrenAge").slider({});*/
+  $("#yourBudget").slider({});
+  $("#childrenAge").slider({});
+});
