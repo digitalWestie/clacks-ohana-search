4b7bab6fc827477084a045398f2332fb5fae7246
Rory Gianni <giannichan@gmail.com>
2014-11-25 17:42:37 +0000

    make sure to make non-sprockets files are compiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Clackskids::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( ie10-viewport-bug-workaround.js leaflet.js bootstrap-slider.js services.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
