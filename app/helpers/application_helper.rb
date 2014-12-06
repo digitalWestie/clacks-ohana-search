e2f52c265a66ae152ba10c40a7e9134a53e6c2bd
Rory Gianni <giannichan@gmail.com>
2014-12-06 20:01:20 +0000

    corrects weekday param values and adds weekdays to no results messsage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  WEEKDAYS = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday).freeze
+
+  def array_to_weekdays(arr)
+    arr.collect { |e| WEEKDAYS[e.to_i - 1] }
+  end
+
 end
