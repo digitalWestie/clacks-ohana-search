536a3143cfd702e4256e7264a0e60444218d8d63
Rory Gianni <giannichan@gmail.com>
2014-12-08 22:57:13 +0000

    adds pagination support

--- config/initializers/clackskids.rb
+++ config/initializers/clackskids.rb
@@ -1,2 +1,7 @@
 CATEGORIES = ["Childcare", "Clubs", "Sport", "Support", "Health", "Learning",
   "Prenatal & Babies", "Music & Drama"]
+
+Kaminari.configure do |config|
+  config.default_per_page = ENV['DEFAULT_PER_PAGE'].to_i
+  config.max_per_page = ENV['MAX_PER_PAGE'].to_i
+end
