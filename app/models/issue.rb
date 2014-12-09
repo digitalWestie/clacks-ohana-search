1c6d807e2b885389f59f86995d67067a99e9ec19
Rory Gianni <giannichan@gmail.com>
2014-12-09 23:26:59 +0000

    sets up issue model validations and attributes

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,9 @@
 class Issue < ActiveRecord::Base
+
+  attr_accessible :reason, :description, :service_id, :service_timestamp
+
+  validates :service_id, :reason, presence: true
+
+  extend Enumerize
+  enumerize :reason, in: [:outdated, :innacurate]
 end
