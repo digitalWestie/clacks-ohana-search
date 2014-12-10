59910a971bb1bbdaa7acf83897b6a216596b7f5d
Rory Gianni <giannichan@gmail.com>
2014-12-10 00:24:20 +0000

    validate issue emails

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,15 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class Issue < ActiveRecord::Base
 
   validates :service_id, :reason, presence: true
+  validates :email, presence: true, email: true
 
   extend Enumerize
   enumerize :reason, in: [:outdated, :innacurate]
