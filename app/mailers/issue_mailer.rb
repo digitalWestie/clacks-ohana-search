c11e6a590acad93b57e9099c3bf2d992bda373ec
Rory Gianni <giannichan@gmail.com>
2014-12-10 13:50:25 +0000

    adds action for alerting admins of issue

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -5,4 +5,8 @@ class IssueMailer < ActionMailer::Base
     @issue = issue
     mail :to => @issue.email, :subject => "Confirm your issue on Clackskids"
   end
+
+  def alert_admins(issue, service)
+    mail :to => service.organisation.admin_emails, :bcc => ENV['SITE_ADMIN'], :subject => "Issue flagged with service: #{service.name} on Clackskids"
+  end
 end
