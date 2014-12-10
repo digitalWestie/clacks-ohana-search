1945310c2adc77a2b8c4cf622a826ef1a3913e2a
Rory Gianni <giannichan@gmail.com>
2014-12-10 13:08:22 +0000

    adds mailer aciton and template for confirming issues

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -1,5 +1,8 @@
 class IssueMailer < ActionMailer::Base
   default from: ENV['FROM_ADDRESS']
 
-
+  def activation(issue)
+    @issue = issue
+    mail :to => @issue.email, :subject => "Confirm your issue on Clackskids"
+  end
 end
