40186c3cc0dbfaab1cb9854a116149d79d67a085
Rory Gianni <giannichan@gmail.com>
2014-12-10 00:27:57 +0000

    simplify email validator

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,15 +1,7 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
-
 class Issue < ActiveRecord::Base
 
-  validates :service_id, :reason, presence: true
-  validates :email, presence: true, email: true
+  validate :service_id, :reason, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
   extend Enumerize
   enumerize :reason, in: [:outdated, :innacurate]
