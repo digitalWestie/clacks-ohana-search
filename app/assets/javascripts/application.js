6cf39bd7f44a7ffa4b90c23755a807c2ce6bbec9
Rory Gianni <giannichan@gmail.com>
2014-12-10 18:58:35 +0000

    installs marker cluster

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap.js
 //= require tooltips.js
+//= require leaflet.js
+//= require leaflet.markercluster.js
+//= require mapping.js
 //= require_tree ./bootstrap
