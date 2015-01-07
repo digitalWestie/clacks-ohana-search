7c6eed27f39fb3cf4c2379ea9b2cdaf343c86d4a
Rory Gianni <giannichan@gmail.com>
2015-01-07 17:12:18 +0000

    renames application to clacks ohana search

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Clackskids::Application.config.session_store :cookie_store, key: '_clackskids_session'
+Clackskids::Application.config.session_store :cookie_store, key: '_clacks_ohana_search_session'
