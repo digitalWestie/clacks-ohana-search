9fa0e3391a8d4dad06e4e799ca4bcd0376844244
Rory Gianni <giannichan@gmail.com>
2015-02-24 16:23:03 +0000

    make sure to deliver issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,7 +4,7 @@ class IssuesController < ApplicationController
     issue = Issue.new(issue_params)
     issue.service_id = params[:service_id]
     if issue.save
-      #send activation email
+      IssueMailer.activation(issue).deliver
       notice = "An email has been sent to #{issue.email}, please check it to confirm this issue."
     else
       notice = "Your issue couldn't be recorded, please try again!"
@@ -15,7 +15,8 @@ class IssuesController < ApplicationController
   def activate
     issue = Issue.find_by(activation: params[:activation])
     if issue and issue.update_attributes(is_activated: true, activation: Time.now.to_i)
-      #send issue email
+      service = Service.get(issue.service_id)
+      IssueMailer.activation(issue, service).deliver
       notice = "Your issue has been noted and our admins contacted, thank you for your help!"
     else
       notice = "Your issue couldn't be handled, please try again!"
