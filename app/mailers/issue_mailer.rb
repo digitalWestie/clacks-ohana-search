9aca87b725c4c477785bb7893310510e09ee2f12
digitalWestie <giannichan@gmail.com>
2015-03-09 15:15:31 +0000

    provides full url to edit service in question

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -9,6 +9,7 @@ class IssueMailer < ActionMailer::Base
   def alert_admins(issue, service)
     @issue = issue
     @service = service
+    @edit_service_url = "#{ENV['OHANA_API_URL']}/admin/organizations/#{service.organization.id}/services/#{service.id}/edit"
     mail :to => ENV['SITE_ADMIN_EMAIL'], :cc => service.admin_emails, :subject => "Issue flagged with service: #{service.name} on #{ENV['APP_TITLE']}"
   end
 end
